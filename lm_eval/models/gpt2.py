5225b9747a014b38f2c2ed0243bd285e9fdefbce
haileyschoelkopf <hailey.schoelkopf@yale.edu>
2023-05-08 19:12:48 -0400

    draft calculator impl.

--- lm_eval/models/gpt2.py
+++ lm_eval/models/gpt2.py
@@ -153,10 +153,80 @@ class HFLM(BaseLM):
 
         if num_return_sequences > 1:
             context = context.expand(num_return_sequences, context.shape[1])
-        return self.gpt2.generate(
+        return self.calculator_generate(
             context, max_length=max_length, eos_token_id=eos_token_id,
             do_sample=do_sample, temperature=temperature
         )
+
+    def calculator_generate(self, context, max_length=None, eos_token_id=None, do_sample=None, temperature=None):
+
+        
+        from contextlib import contextmanager
+        import signal
+        import torch as th
+
+        def use_calculator(sample):
+            if "<<" not in sample:
+                return None
+
+            parts = sample.split("<<")
+            remaining = parts[-1]
+            if ">>" in remaining:
+                return None
+            if "=" not in remaining:
+                return None
+            lhs = remaining.split("=")[0]
+            lhs = lhs.replace(",", "")
+            if any([x not in "0123456789*+-/.()" for x in lhs]):
+                return None
+            try:
+                return ast.literal_eval(lhs)
+            except:
+                return None
+
+        ctx = context
+        for _ in range(max_length - context.shape[1]):
+            out = self.gpt2.generate(
+                ctx, max_length=ctx.shape[1] + 1, eos_token_id=eos_token_id,
+                do_sample=do_sample, temperature=temperature, 
+            )
+            text = self.tokenizer.batch_decode(out)[0]
+            if "=" in text[-2:]: # hacky: 
+                print(text)
+                answer = use_calculator(text)
+                if answer is not None:
+                    print(f"Triggered calculator on text {text}, answer", answer)
+                    text = text + str(answer) + ">>"
+                    ctx = self.tokenizer([text], padding=False, return_tensors="pt").to(self._device)
+                else: 
+                    ctx = out
+            else:
+                ctx = out
+
+        return ctx
+        # def sample(model, qn, tokenizer, device, sample_len):
+        #     # Inefficient version of calculator sampling -- no batches, doesn't
+        #     # cache activations from previous tokens
+        #     EQUALS_TOKENS = set([28, 796, 47505])
+
+        #     for _ in range(sample_len):
+        #         with th.no_grad():
+        #             toks = tokenizer([qn], padding=False, return_tensors="pt").to(device)
+        #             orig_len = toks["input_ids"].shape[1]
+
+        #             out = model.generate(
+        #                 **toks, max_length=orig_len + 1, pad_token_id=model.config.eos_token_id
+        #             )
+        #             text = tokenizer.batch_decode(out)[0]
+
+        #             if out[0, -1].item() in EQUALS_TOKENS:
+        #                 answer = use_calculator(text)
+        #                 if answer is not None:
+        #                     print("Triggered calculator, answer", answer)
+        #                     text = text + str(answer) + ">>"
+
+        #             qn = text
+        #     return qn
 
 
 # for backwards compatibility
