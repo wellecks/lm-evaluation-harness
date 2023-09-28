574a69d2ada272afcac469c48662ce28054427d6
zhangir-azerbayev <zazerbayev@gmail.com>
2023-09-28 06:52:04 +0000

    bug fixes

--- eval_scripts/eval_7b_majk.sh
+++ eval_scripts/eval_7b_majk.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 HARNESS_DIR=/fsx/proj-mathlm/lm-evaluation-harness-dev
+cd $HARNESS_DIR
 
 models=("/fsx/proj-mathlm/downloaded-weights/Llama-2-7b-hf" "/fsx/proj-mathlm/downloaded-weights/CodeLlama-7b-hf" "/fsx/proj-mathlm/downloaded-weights/llemma_7b") 
 model_names=("llama-2_7b" "codellama_7b" "llemma_7b")
@@ -33,8 +34,8 @@ for i in {0..2}; do
     MODEL_NAME=${model_names[$i]}
 
     for j in {0..9}; do
-        TASK=${tasks[$i]}
-        TASK_NAME=${task_names[$i]}
+        TASK=${tasks[$j]}
+        TASK_NAME=${task_names[$j]}
 
         OUT=${HARNESS_DIR}/output/${MODEL_NAME}_${TASK_NAME}_majk.json
 
@@ -47,8 +48,8 @@ for i in {0..2}; do
 #SBATCH --cpus-per-task=12          # Number of cores per tasks
 #SBATCH --mem-per-cpu=64G
 #SBATCH --gres=gpu:1                 # Number of gpus
-#SBATCH --output=${MODEL_NAME}_${TASK_NAME}_majk.out      # Set this dir where you want slurm outs to go
-#SBATCH --error=${MODEL_NAME}_${TASK_NAME}_majk.out      # Set this dir where you want slurm outs to go
+#SBATCH --output=slurmouts/7b_majk/${MODEL_NAME}_${TASK_NAME}_majk.out      # Set this dir where you want slurm outs to go
+#SBATCH --error=slurmouts/7b_majk/${MODEL_NAME}_${TASK_NAME}_majk.out      # Set this dir where you want slurm outs to go
 #SBATCH --account=neox
 #SBATCH --open-mode=append
 #SBATCH --requeue
@@ -66,7 +67,7 @@ source ${HARNESS_DIR}/eval_scripts/env.sh
 ### end configure environment
 
 # if testing, uncomment --limit for testing
-python main.py --no_cache --model vllm --model_args pretrained=${MODEL} --tasks $TASK --output_path ${OUT} --tp_degree 1 --description_dict_path ${HARNESS_DIR}/configs/majk.json --limit 3
+python main.py --no_cache --model vllm --model_args pretrained=${MODEL} --tasks $TASK --output_path ${OUT} --tp_degree 1 --description_dict_path ${HARNESS_DIR}/configs/majk.json
 EOL
         
         sbatch temp.sh
